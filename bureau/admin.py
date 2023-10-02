4018ae464557b15952212656ea0d11644f3172ec
Naholiuk Yevhen <e.nagolyuk@gmail.com>
2023-10-02 09:19:15 +0300

    Added app to Settings and finished work with files models.py and admin.py

--- bureau/admin.py
+++ bureau/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Topic, Redactor, NewsPaper
 
-# Register your models here.
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Redactor)
+class EditorAdmin(admin.ModelAdmin):
+    list_display = ('username', 'years_of_experience',)
+
+
+@admin.register(NewsPaper)
+class NewsPaperAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_year', 'topic',)
+    filter_horizontal = ('publishers',)
--- bureau/models.py
+++ bureau/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+
+class NewsPaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_year = models.DateField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name="newspapers")
+    publishers = models.ManyToManyField(Redactor)
+
+    def __str__(self):
+        return self.title
