b8510188dfac5008ce4d0d458560ba7c2eec1fc5
Naholiuk Yevhen <e.nagolyuk@gmail.com>
2023-10-04 12:57:02 +0300

    Fixed urls imports

--- news_flow_hub/urls.py
+++ news_flow_hub/urls.py
@@ -18,10 +18,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("bureau.urls", namespace="bureau")),
@@ -30,4 +26,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
