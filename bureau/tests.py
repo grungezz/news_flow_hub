9c1f32b39d61eb8f8f9bbb1a5baedebfd0b4ebe2
Naholiuk Yevhen <e.nagolyuk@gmail.com>
2023-10-10 14:31:49 +0300

    Added .idea to gitignore

--- bureau/tests.py
+++ bureau/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
+from bureau.models import Topic
+
+
 # Create your tests here.
+class ModelCreateTestCase(TestCase):
+    def test_create_topic(self):
+        topic = Topic.objects.create(name="hello")
+        self.assertEqual(topic.name, "hello")
